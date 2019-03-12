c8a603eb5b23a54853e87f97148e290baef3866b
tsk-nkgw <tasktas9@gmail.com>
2019-03-12 23:59:56 +0900

    replace deque

--- src/main/java/lifegame/BoardModel.java
+++ src/main/java/lifegame/BoardModel.java
@@ -1,33 +1,38 @@
 package lifegame;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class BoardModel {
-	final private boolean[][] cells;
+	private boolean[][] board;
 	final private int col;
 	final private int row;
 	final private List<BoardListener> listeners;
-	
-	public BoardModel(final int col,final int row) {
+	final private Deque<boolean[][]> boardHistory;
+	final static private int MAX_STACK_SIZE = 30;
+
+	public BoardModel(final int col, final int row) {
 		this.col = col;
 		this.row = row;
-		this.cells = new boolean[row][col];
+		this.board = new boolean[row][col];
 		this.listeners = new ArrayList<>();
+		this.boardHistory = new ArrayDeque<>(MAX_STACK_SIZE);
 	}
-	
+
 	public void addListeners(final BoardListener listener) {
 		this.listeners.add(listener);
 	}
-	
+
 	private void fireUpdate() {
-		for(final BoardListener listener : this.listeners) {
+		for (final BoardListener listener : this.listeners) {
 			listener.update(this);
 		}
 	}
-	
-	public void changeCellState(final int x,final int y) {
-		this.cells[x][y] = !this.cells[x][y];
+
+	public void changeCellState(final int x, final int y) {
+		this.board[x][y] = !this.board[x][y];
 		fireUpdate();
 	}
 
@@ -38,8 +43,25 @@ public class BoardModel {
 	public int getRow() {
 		return this.row;
 	}
-	
+
 	public void next() {
-		
+		final boolean[][] nextBoard = new boolean[row][col];
+
+		if (this.boardHistory.size() > MAX_STACK_SIZE - 1) {
+			throw new RuntimeException("invalid");
+		} else if (this.boardHistory.size() == MAX_STACK_SIZE - 1) {
+			boardHistory.removeFirst();
+		}
+
+		this.boardHistory.addLast(this.board.clone());
+		this.board = nextBoard;
+	}
+	
+	public void undo() {
+		this.board = this.boardHistory.getLast();
+	}
+	
+	public int isUndoable() {
+		return this.boardHistory.size();
 	}
 }
