63c3ac4b67d2c043a5ce8a00924c590ffc8a1ca6
tsk-nkgw <tasktas9@gmail.com>
2019-03-13 13:47:56 +0900

    create window

--- src/main/java/lifegame/Main.java
+++ src/main/java/lifegame/Main.java
@@ -1,9 +1,32 @@
 package lifegame;
 
-public class Main {
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public class Main implements Runnable {
+	final static private String WINDOW_TITLE = "lifegame";
+	final static private int MINIMUM_WIDTH = 300;
+	final static private int MINIMUM_HEIGHT = 200;
 
 	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Main());
+	}
 
+	public void run() {
+		JFrame frame = new JFrame();
+		frame.setTitle(WINDOW_TITLE);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		JPanel base = new JPanel();
+		frame.setContentPane(base);
+		base.setPreferredSize(new Dimension(400, 300));
+		frame.setMinimumSize(new Dimension(MINIMUM_WIDTH, MINIMUM_HEIGHT));
+
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 }
