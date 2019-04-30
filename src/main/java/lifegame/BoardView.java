b9955fd7354de4dfb9258eba12ecd570f49f406a
tsk-nkgw <tasktas9@gmail.com>
2019-04-30 23:28:56 +0900

    [test]imple override method

--- src/main/java/lifegame/BoardView.java
+++ src/main/java/lifegame/BoardView.java
@@ -1,7 +1,14 @@
 package lifegame;
 
 import javax.swing.JPanel;
+import java.awt.*;
 
 public class BoardView extends JPanel{
+    @Override
+    public void paint(final Graphics g) {
+        super.paint(g);
 
+        g.drawLine(20, 30, 50, 30);
+        g.fillRect(31,31,9,9);
+    }
 }
